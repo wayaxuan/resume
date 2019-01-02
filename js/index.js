5cdb59bfa21468e919b277d23c4270a5b738719a
wayaxuan <1105663645@qq.com>
2019-01-02 15:44:32 +0800

    1
    1

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//var soucee;
+//$(".main7>span>a").on("click",function(){
+//	soucee=$(this).attr("ind");
+//	alert(soucee);
+//	
+//})
+//$(".play audio source").attr("src","../audio/"+soucee+"");
+//
+//alert($(".play audio source").attr("src"));
